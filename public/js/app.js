58599e376f0425f7f6964cdad5ff2220d0500d78
Alec Wilbur <lawilbur2@gmail.com>
2018-05-04 15:58:54 -0400

    basic react setup

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+class App extends React.Component {
+	render () {
+		return (
+			<div>
+				<h1>Magic The Gathering</h1>
+
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<App />,
+	document.querySelector('.container')
+);
