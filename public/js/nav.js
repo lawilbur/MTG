b41da9b4c3b79d03155cb246a30029c4e3afc64c
Alec Wilbur <lawilbur2@gmail.com>
2018-05-09 15:30:39 -0400

    switching to angularjs frontend

---
+++ public/js/nav.js
@@ -0,0 +1,19 @@
+// class Navbar extends React.Component {
+// 	render() {
+// 		return (
+// 			<nav className="navbar navbar-dark bg-dark">
+// 				<div className="navbar-nav">
+// 				<a className="nav-item nav-link" href="#">Signup</a>
+// 				</div>
+// 				<form class="form-inline">
+//   					<input class="form-control mr-sm-1" type="search" placeholder="Search" aria-label="Search"></input>
+//   					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+// 				</form>
+// 				<div className="navbar-nav">
+// 				<a className="nav-item nav-link" href="#">Login</a>
+// 				</div>
+//
+// 			</nav>
+// 		)
+// 	}
+// }
